25cd6313db4d712e178a1f97ed827d28f03d3d0d
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-10 17:17:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,14 @@
 require 'pry'
 require_relative "./user.rb"
 class Student < User
-  
+  @@knowledge = []
   def initialize
-    @@knowledge = []
+    @@knowledge 
   end 
   
   def learn(argument)
-    knowledge << argument 
-    binding.pry 
+    @@knowledge << argument 
+    #binding.pry 
   end 
   
 end
